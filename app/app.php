1747bb5f0e0cd611409327891d359f5ae92b03e5
Clayton Collins and Philip Putnam <philip@>
2017-02-21 12:03:33 -0800

    add twig, forms, and routes

--- app/app.php
+++ app/app.php
@@ -4,12 +4,37 @@
     require_once __DIR__."/../src/Animal.php";
     require_once __DIR__."/../src/Type.php";
 
+    $server = 'mysql:host=localhost:8889;dbname=shelter';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), ["twig.path" => __DIR__."/../views"]);
+    $app['debug'] = true;
 
     $app->get('/', function() use($app) {
-        $result = 'hello';
-        return $app["twig"]->render("root.html.twig", ['result' => $result]);
+        $animals = Animal::getAll();
+        $types = Type::getAll();
+        return $app["twig"]->render("root.html.twig", ['animals'=>$animals,'types'=>$types]);
+    });
+
+    $app->post('/add-animal', function() use($app) {
+        $id = null;
+        $new_animal = new Animal($_POST['name'],$_POST['gender'],$_POST['admit_date'],$_POST['breed'], $_POST['type_id'], $id);
+        $new_animal->save();
+        $animals = Animal::getAll();
+        $types = Type::getAll();
+        return $app["twig"]->render("root.html.twig", ['animals'=>$animals,'types'=>$types]);
+    });
+
+    $app->post('/add-type', function() use($app) {
+        $id = null;
+        $new_type = new Type($_POST['name'], $id);
+        $new_type->save();
+        $animals = Animal::getAll();
+        $types = Type::getAll();
+        return $app["twig"]->render("root.html.twig", ['animals'=>$animals,'types'=>$types]);
     });
 
     return $app;
